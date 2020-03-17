2e626053b42bc736f136b0b2b8eabfa8b08ba178
Tomasz Witczak <TomaszMWitczak@gmail.com>
2020-03-17 20:39:01 +0100

    Add first draft

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        currentForm = humanForm;
+    }
+
+    void Update()
+    {
+        inputLaneDown = Input.GetKeyDown("s");
+        inputLaneUp = Input.GetKeyDown("w");
+
+        // Change current lane
+        currentLane += inputLaneUp ? 1 : 0;
+        currentLane -= inputLaneDown ? 1 : 0;
+
+        if(Input.GetKeyDown("e")){
+            GameObject.Instantiate(explosionPrefab, currentForm.transform.position,
+            Quaternion.identity);
+
+            if (currentForm == humanForm) {
+                currentForm = eagleForm;
+
+                eagleForm.transform.position = new Vector3(humanForm.transform.position.x,
+                    eagleForm.transform.position.y, humanForm.transform.position.z);
+
+                eagleForm.SetActive(true);
+                humanForm.SetActive(false);
+
+                mainCamera.player = eagleForm;
+                mainCamera.cameraTarget = eagleForm;
+                mainCamera.SetPosition();
+            }
+            else if (currentForm == eagleForm) {
+                currentForm = humanForm;
+
+                humanForm.transform.position = new Vector3(eagleForm.transform.position.x,
+                    humanForm.transform.position.y, eagleForm.transform.position.z);
+
+                eagleForm.SetActive(false);
+                humanForm.SetActive(true);
+
+                mainCamera.player = humanForm;
+                mainCamera.cameraTarget = humanForm;
+                mainCamera.SetPosition();
+            }
+        }
+
+        // Move only in lane increments
+        moveInput.x = 1.0f;//Mathf.Clamp(Input.GetAxisRaw("Horizontal") + 1.0f, 0.5f, 1.5f);
+        moveInput.y = -Input.GetAxisRaw("Horizontal");
+        moveInput.z = (new Vector3(currentLane * laneWidth - currentForm.transform.position.z, 0.0f, 0.0f)).x;
+
+        if (currentForm == humanForm)
+        {
+            moveInput.y = 0.0f;
+
+			humanForm.GetComponent<RPGCharacterAnims.RPGCharacterInputControllerFREE>().inputHorizontal = Input.GetAxisRaw("Horizontal");
+			humanForm.GetComponent<RPGCharacterAnims.RPGCharacterInputControllerFREE>().inputVertical = Input.GetAxisRaw("Vertical");
+			humanForm.GetComponent<RPGCharacterAnims.RPGCharacterInputControllerFREE>().moveInput = moveInput;
+        }
+        else if (currentForm == eagleForm)
+        {
+            eagleForm.GetComponent<DemoController>().horizontal = Input.GetAxisRaw("Horizontal");
+            eagleForm.GetComponent<DemoController>().vertical = Input.GetAxisRaw("Vertical");
+
+            currentVelocity = Vector3.MoveTowards(currentVelocity, moveInput * runSpeed, movementAcceleration * Time.deltaTime);
+			eagleForm.transform.position += currentVelocity * Time.deltaTime;
+        }
+
+        // Update torch position
+        torch.transform.position = new Vector3(
+            currentForm.transform.position.x,
+            torch.transform.position.y,
+            currentForm.transform.position.z - 2.0f
+        );
+
+    }
+
+    public float movementAcceleration = 90.0f;
+    public float runSpeed = 6f;
+
+	public Vector3 currentVelocity;
+    public int currentLane;
+    public int laneWidth;
+    public GameObject humanForm;
+    public GameObject eagleForm;
+    public GameObject explosionPrefab;
+    public CameraController mainCamera;
+    public GameObject torch;
+
+    private GameObject currentForm;
+
+		[HideInInspector] public bool inputLaneUp;
+		[HideInInspector] public bool inputLaneDown;
+
+    private Vector3 moveInput;
+
+}
---
+++ Assets/RPG Character Animation Pack FREE/Code/CameraController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject cameraTarget;
+	public float rotateSpeed;
+	float rotate;
+	public float offsetDistance;
+	public float offsetHeight;
+	public float smoothing;
+	Vector3 offset;
+	bool following = true;
+	Vector3 lastPosition;
+	public GameObject player;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		cameraTarget = player;
+		// lastPosition = new Vector3(cameraTarget.transform.position.x + 5.0f, cameraTarget.transform.position.y + offsetHeight, cameraTarget.transform.position.z - offsetDistance);
+		// offset = new Vector3(cameraTarget.transform.position.x + 5.0f, cameraTarget.transform.position.y + offsetHeight, cameraTarget.transform.position.z - offsetDistance);
+		offset = transform.position - player.transform.position;
+	}
+
+	public void SetPosition() {
+		// lastPosition = new Vector3(cameraTarget.transform.position.x + 5.0f, cameraTarget.transform.position.y + offsetHeight, cameraTarget.transform.position.z - offsetDistance);
+		// offset = new Vector3(cameraTarget.transform.position.x + 5.0f, cameraTarget.transform.position.y + offsetHeight, cameraTarget.transform.position.z - offsetDistance);
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.F))
+		{
+			if(following)
+			{
+				following = false;
+			} 
+			else
+			{
+				following = true;
+			}
+		} 
+		// if(Input.GetKey(KeyCode.Q))
+		// {
+		// 	rotate = -1;
+		// } 
+		// else if(Input.GetKey(KeyCode.E))
+		// {
+		// 	rotate = 1;
+		// } 
+		// else
+		// {
+		// 	rotate = 0;
+		// }
+		// if(following)
+		// {
+		Vector3 temp = Vector3.zero;
+
+		// transform.position = Vector3.Lerp(lastPosition, cameraTarget.transform.position + offset,
+		// ref temp, 0.1f);
+		transform.position = new Vector3(Mathf.Lerp(lastPosition.x, cameraTarget.transform.position.x + offset.x, smoothing * Time.deltaTime), 
+			Mathf.Lerp(lastPosition.y, cameraTarget.transform.position.y + offset.y, smoothing * Time.deltaTime), 
+			Mathf.Lerp(lastPosition.z, cameraTarget.transform.position.z + offset.z, smoothing * Time.deltaTime));
+		// } 
+		// else
+		// {
+		// 	transform.position = lastPosition; 
+		// }
+		// transform.LookAt(new Vector3(cameraTarget.transform.position.x,
+		// cameraTarget.transform.position.y, cameraTarget.transform.position.z));
+	}
+
+	void LateUpdate()
+	{
+		lastPosition = transform.position;
+	}
+}
